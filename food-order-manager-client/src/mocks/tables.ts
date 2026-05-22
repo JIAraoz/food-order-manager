5c954a116258256942963df879cd3cdc14b84ce3
JIAraoz <ignacioaraoz18@gmail.com>
2026-05-22 16:11:09 -0300

    style-mock

---
+++ food-order-manager-client/src/mocks/tables.ts
@@ -0,0 +1,20 @@
+export const tables = [
+  {
+    id: 1,
+    number: 1,
+    status: "occupied",
+    total: 15000,
+  },
+  {
+    id: 2,
+    number: 2,
+    status: "free",
+    total: 0,
+  },
+  {
+    id: 3,
+    number: 3,
+    status: "preparing",
+    total: 8200,
+  },
+]
