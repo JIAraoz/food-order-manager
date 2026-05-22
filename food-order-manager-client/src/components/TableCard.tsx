18564caddd6583d775921da8c06cb851eb9ec847
JIAraoz <ignacioaraoz18@gmail.com>
2026-05-22 16:29:15 -0300

    feat-tables

--- food-order-manager-client/src/components/TableCard.tsx
+++ food-order-manager-client/src/components/TableCard.tsx
@@ -1,12 +1,28 @@
 import type { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
-export default function TableCard(table: { number: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; status: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; total: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }){
+
+const statusStyles = {
+  free: "bg-emerald-500",
+  occupied: "bg-red-500",
+  preparing: "bg-amber-500",
+} as const
+
+type Status = keyof typeof statusStyles;
+
+type TableCardProps = {
+    number: string | number;
+    status: Status;
+    total: string | number;
+};
+
+export default function TableCard({ number, status, total }: TableCardProps){
+
     return(
 
-        <div className="tableCard bg-zinc-900 border-zinc-800 rounded-2xl p-4">
-            <h3>Table {table.number}</h3>
-            <span>{table.status}</span>
-            <span>{table.total}</span>
+        <div className="tableCard bg-zinc-900 border-zinc-800 rounded-2xl p-4 flex flex-col gap-2 m-5">
+            <h3>Table {number}</h3>
+            <span className={`${statusStyles[status] || "bg-gray-500" } rounded-2xl `}>{status}</span>
+            <span>Total: {total}</span>
         </div>
     )
 }
